d0eb1408f8a94021f5a6f344e6d1e20f310e3540
Yann Laurain <ylaurain@gmail.com>
2020-05-07 18:14:16 +0200

    mise à jour

--- error.php
+++ error.php
@@ -3,7 +3,10 @@
 require_once "log.php";
 
 function pageHasError($url){
+
 	$error = getElementByClassName($url,'table','xdebug-error xe-notice');
+
+	if ($error == '') $error = getElementByClassName($url,'table','xdebug-error xe-warning');
 
 	showLog('framework/error.php', 'pageHasError', $error);
 
